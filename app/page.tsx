c7dcd6971365dbb3fdf8f4a5869d6a0e2b8dbf3d
Emranhossainshuvo <emran.shuvo.dev@gmail.com>
2024-08-03 19:14:22 +0600

    createed home.tsx file to work with everything related to home page and added it in the page.tsx aslo added data file where all the json file stored

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
+import Home from "../components/Home/Home"
 
-const Home = () => {
+
+const HomePage = () => {
     return (
         <div>
-                <h2 className="text-6xl text-red-700">Home page it is</h2>
+            <h2 className="text-6xl text-red-700">
+                <Home />
+            </h2>
         </div>
     );
 };
 
-export default Home;
+export default HomePage;
