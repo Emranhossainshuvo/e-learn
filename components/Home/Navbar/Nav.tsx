cc8f3fc54934aca2b176becfe61ca06e916bb519
Emranhossainshuvo <emran.shuvo.dev@gmail.com>
2024-08-08 19:19:28 +0600

    created the navbar with proper styling

--- components/Home/Navbar/Nav.tsx
+++ components/Home/Navbar/Nav.tsx
@@ -1,4 +1,6 @@
+import { Navlink } from "@/constant/constant";
 import Image from "next/image";
+import Link from "next/link";
 
 const Nav = () => {
     return (
@@ -6,7 +8,11 @@ const Nav = () => {
             <div className="flex items-center justify-between w-[90%] xl:w-[80%] mx-auto">
                 <Image src={"/images/icons8-shop-100.png"} width={120} height={120} alt="website logo icon"></Image>
                 <div className="hidden items-center space-x-10 lg:flex">
-                    
+                    {Navlink.map((link) => {
+                        return <Link key={link.id} href={link.url}>
+                            <p className="nav__link">{link.label}</p>
+                        </Link>
+                    })}
                 </div>
             </div>
         </div>
