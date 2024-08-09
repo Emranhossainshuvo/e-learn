b140c4bb3d0a8cb5aee9c8e5445535b0af1c045a
Emranhossainshuvo <emran.shuvo.dev@gmail.com>
2024-08-09 10:20:38 +0600

    created function to handle navbar dynamic color change

--- components/Home/Navbar/Nav.tsx
+++ components/Home/Navbar/Nav.tsx
@@ -1,8 +1,28 @@
+"use client";
+
 import { Navlink } from "@/constant/constant";
 import Image from "next/image";
 import Link from "next/link";
+import { useEffect, useState } from "react";
+import { HiBars3BottomRight } from "react-icons/hi2";
 
 const Nav = () => {
+
+    const [navBg, setNavBg] = useState(false);
+
+    useEffect(() => {
+        const handler = () => {
+            if (window.scrollY >= 90) {
+                setNavBg(true);
+            };
+            if (window.scrollY < 90) {
+                setNavBg(false);
+            };
+        }
+        window.addEventListener("scroll", handler)
+
+    }, [])
+
     return (
         <div className="fixed w-full transition-all duration-150 h-[12vh] z-[1000] bg-blue-700">
             <div className="flex items-center justify-between w-[90%] xl:w-[80%] mx-auto">
@@ -13,6 +33,10 @@ const Nav = () => {
                             <p className="nav__link">{link.label}</p>
                         </Link>
                     })}
+                </div>
+                <div className="flex items-center space-x-4">
+                    <button className="md:px-10 md:py-2 px-8 py-1.5 text-white font-semibold text-base bg-pink-700 hover:bg-pink-900 transition-all duration-200 rounded-lg">Signup now</button>
+                    <HiBars3BottomRight className="w-8 h-8 cursor-pointer text-white lg:hidden" />
                 </div>
             </div>
         </div>
