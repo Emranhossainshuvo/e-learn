5ab1c836e849eba1efded91694e2116289dd71da
Emranhossainshuvo <emran.shuvo.dev@gmail.com>
2024-08-09 19:58:59 +0600

    defined the types for some functions and created funciton for open and close navbar for the mobile device

--- components/Home/Navbar/Nav.tsx
+++ components/Home/Navbar/Nav.tsx
@@ -6,7 +6,11 @@ import Link from "next/link";
 import { useEffect, useState } from "react";
 import { HiBars3BottomRight } from "react-icons/hi2";
 
-const Nav = () => {
+type Props = {
+    openNav: () => void;
+}
+const Nav = ({openNav}:Props) => {
+
 
     const [navBg, setNavBg] = useState(false);
 
@@ -26,7 +30,7 @@ const Nav = () => {
     }, [])
 
     return (
-        <div className={`fixed ${navBg ? "bg-indigo-800" :"fixed"} w-full transition-all duration-150 h-[12vh] z-[1000] bg-blue-700`}>
+        <div className={`fixed ${navBg ? "bg-indigo-800" : "fixed"} w-full transition-all duration-150 h-[12vh] z-[1000] bg-blue-700`}>
             <div className="flex items-center justify-between w-[90%] xl:w-[80%] mx-auto">
                 <Image src={"/images/icons8-shop-100.png"} width={120} height={120} alt="website logo icon"></Image>
                 <div className="hidden items-center space-x-10 lg:flex">
@@ -38,7 +42,7 @@ const Nav = () => {
                 </div>
                 <div className="flex items-center space-x-4">
                     <button className="md:px-10 md:py-2 px-8 py-1.5 text-white font-semibold text-base bg-pink-700 hover:bg-pink-900 transition-all duration-200 rounded-lg">Signup now</button>
-                    <HiBars3BottomRight className="w-8 h-8 cursor-pointer text-white lg:hidden" />
+                    <HiBars3BottomRight onClick={openNav} className="w-8 h-8 cursor-pointer text-white lg:hidden" />
                 </div>
             </div>
         </div>
