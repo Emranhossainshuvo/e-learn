a0047e4ff4c6e6d1de7ec0cf1c6ca8a776fa8bbb
Emranhossainshuvo <emran.shuvo.dev@gmail.com>
2024-08-13 10:13:03 +0600

    courses displayed with it's all contents

--- components/Home/Courses/CourseCard.tsx
+++ components/Home/Courses/CourseCard.tsx
@@ -1,4 +1,6 @@
 import Image from "next/image";
+import { FaFile, FaStar, FaUser } from "react-icons/fa";
+import { FaUserGroup } from "react-icons/fa6";
 
 
 // defining a type 
@@ -22,6 +24,36 @@ const CourseCard = ({ course }: Props) => {
             <div className="bg-white rounded-lg overflow-hidden cursor-pointer">
                 <div>
                     <Image src={course.image} alt={course.title} width={400} height={400} className="h-full w-full" />
+                </div>
+                <div className="p-4">
+                    <h1 className="ml-auto relative z-[10] h-20 w-20 flex items-center text-lg font-bold justify-center flex-col mt-[-4rem] rounded-full bg-rose-700 text-white">${course.price}</h1>
+                    <div className="flex items-center mt-6 space-x-4">
+                        <span className="text-lg text-black text-opacity-70 font-bold">{course.category}</span>
+                        <span className="text-base text-gray-600">{course.author}</span>
+                    </div>
+                    <h1 className="text-xl text-black font-bold mt-2">{course.title}</h1>
+                    <div className="flex mt-2 items-center space-x-2">
+                        <div className="flex items-center">
+                            <FaStar className="h-4 w-4 text-yellow-600" />
+                            <FaStar className="h-4 w-4 text-yellow-600" />
+                            <FaStar className="h-4 w-4 text-yellow-600" />
+                            <FaStar className="h-4 w-4 text-yellow-600" />
+                            <FaStar className="h-4 w-4 text-yellow-600" />
+                        </div>
+                        <span className="text-base text-orange-800 font-semibold">({course.reviewNumber} Reviews)</span>
+                    </div>
+                    <div className="my-6 w-full h-[2px] bg-gray-500 opacity-15">
+                    </div>
+                    <div className="flex mb-8 items-center justify-between">
+                        <div className="flex items-center space-x-2">
+                            <FaFile className="w-4 h-4 text-orange-600" />
+                            <p className="text-base font-semibold text-gray-800">{course.lessons} Lessons</p>
+                        </div>
+                        <div className="flex items-center space-x-2">
+                            <FaUserGroup className="w-4 h-4 text-orange-600" />
+                            <p className="text-base font-semibold text-gray-800">{course.students} Students</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
--- components/Home/Courses/Courses.tsx
+++ components/Home/Courses/Courses.tsx
@@ -8,7 +8,7 @@ const Courses = () => {
             <div className="pt-16 pb-12 relative bg-gray-200">
                 <div className="w-[80%] pt-8 pb-8 mx-auto">
                     <h1 className="text-4xl md:text-5xl text-gray-900 font-bold">Popular courses</h1>
-                    <div className="mt-10 md:mt-16 grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-10">
+                    <div className="mt-10 md:mt-16 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
                         {coursesData.map((course)=>{
                             return <div key={course.id}>
                                 <CourseCard course={course} />
